e2e07511073e981947475972e271f1a4957e706a
Kevin Burns <kevin.burns@steamrollerstudios.com>
2023-06-07 18:06:01 -0400

    Began refactoring to use a Postgres database for job tracking

--- flaskTest.py
+++ flaskTest.py
@@ -1,12 +1,30 @@
 from flask import Flask, request, Response, render_template
+from flask_cors import CORS
+import sys
 import json
 import os
 import random
 import subprocess
 from enum import Enum, IntEnum
+import socket
+
 from steamroller.plasticAPI.workspace import Workspace
 
+sys.path.insert(0, r'{}\Lib\site-packages'.format(os.environ['CONDA_PREFIX']))
+import psycopg2
+import psycopg2.extras
+from psycopg2.extensions import AsIs
+
 app = Flask(__name__)
+CORS(app)
+
+pipeline_db = psycopg2.connect(database="pipeline",
+                        host="10.100.0.48",
+                        user="steamroller",
+                        password="!uQ7Br&wratR",
+                        port="5432")
+
+dbcursor = pipeline_db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
 runningWorkflows = dict()
 
@@ -18,10 +36,82 @@ class StatusCode(IntEnum):
     FAILED = -2
 
 class ConstantPaths(str, Enum):
-    LUIGI_TASK_PATH = r"D:\SteamrollerDev\developerpackages\steamroller\steamroller.tools.publisher\python\steamroller\tools\publisher"
+    LUIGI_TASK_PATH = r"D:\SteamrollerDev\developerpackages\steamroller\steamroller.tools.publisher\python\steamroller\tools\publisher",
+    PIPELINE_JOBS_TABLE = 'public."Pipeline_Jobs"'
+
+HOSTNAME = socket.gethostname()
+dbcursor.execute('SELECT * FROM {}'.format(ConstantPaths.PIPELINE_JOBS_TABLE))
+PIPELINE_COLUMNS = tuple(col.name for col in dbcursor.description)
+
+def deleteJobFromDb(type, jobId):
+    dbcursor.execute("DELETE FROM {} WHERE jobid = {} AND jobtype = '{}'".format(ConstantPaths.PIPELINE_JOBS_TABLE, jobId, type))
+    pipeline_db.commit()
+
+def updateLocalWorkflowInfo():
+    dbcursor.execute('SELECT * FROM {}'.format(ConstantPaths.PIPELINE_JOBS_TABLE))
+    results = dbcursor.fetchall()
+    for workflow in results:
+        key = "{}_{}".format(workflow['jobtype'].strip(), workflow['jobid'])
+        entry = runningWorkflows[key] if key in runningWorkflows else dict()
+        runningWorkflows[key] = entry
+        for column in workflow:
+            entry[column] = workflow[column].strip() if isinstance(workflow[column], str) else workflow[column]
+    pipeline_db.commit()
+
+def ensureCorrectColumns(data):
+    keys = data.keys()
+    for key in keys:
+        if not key in PIPELINE_COLUMNS:
+            data.pop(key)
+    return data
+
+def getUpdatedDefaultWorkflow(type, jobId, data):
+    result = {
+        'jobid': jobId,
+        'jobtype': type,
+        'jobname': '{}_publish'.format(type),
+        'jobstatus': 0,
+        'machinename': HOSTNAME,
+        'triggeredby': 'Anonymous',
+        'taskstatus': 'Running...',
+        'executorip': '0.0.0.0'
+    }
+    result.update(data)
+    return result
+
+def updateRemoteWorkflowInfo(type, jobId, data, includeDefaults = False):
+    if includeDefaults:
+        result = getUpdatedDefaultWorkflow(type, jobId, data)
+    else:
+        result = dict()
+        result.update(data)
+    result = ensureCorrectColumns(result)
+    columns = result.keys()
+    values = tuple(result[column] for column in columns)
+    statement = "INSERT INTO {} (%s) VALUES %s".format(ConstantPaths.PIPELINE_JOBS_TABLE)
+    prepared_statement = dbcursor.mogrify(statement, (AsIs(', '.join(columns)), values))
+    dbcursor.execute(prepared_statement)
+    pipeline_db.commit()
 
 def getStatus(key):
-    return runningWorkflows[key]['process'].poll() if key in runningWorkflows else -1
+    if key not in runningWorkflows:
+        return -1
+    elif 'process' not in runningWorkflows[key]:
+        return runningWorkflows[key]['jobstatus']
+    else:
+        status = runningWorkflows[key]['process'].poll()
+        return status
+
+def getJobParamFilename(type, jobId):
+    if type == 'model':
+        return os.path.join(ConstantPaths.LUIGI_TASK_PATH, 'model_publish_{}.json'.format(jobId))
+    return ''
+
+def removeQuiet(fn):
+    try:
+        os.remove(fn)
+    except:
+        pass
 
 @app.route('/')
 def taskUI():
@@ -52,42 +142,51 @@ def indexTest():
 
 @app.route('/api/getTrackedJobs')
 def getTrackedJobs():
+    updateLocalWorkflowInfo()
     returnDict = dict()
     for (key, workflow) in runningWorkflows.items():
         returnDict[key] = workflow.copy()
         status = getStatus(key)
-        print(key, status)
         if status is None:
-            returnDict[key]['status'] = StatusCode.RUNNING
+            returnDict[key]['jobstatus'] = StatusCode.RUNNING
         elif status != 0:
-            returnDict[key]['status'] = StatusCode.FAILED
+            returnDict[key]['jobstatus'] = StatusCode.FAILED
         else:
-            returnDict[key]['status'] = StatusCode.SUCCEEDED
+            returnDict[key]['jobstatus'] = StatusCode.SUCCEEDED
+        returnDict[key]['links'] = {
+            'get_status': '/api/jobStatus/{}/{}'.format(returnDict[key]['jobtype'], returnDict[key]['jobid']),
+            'kill_job': '/api/killJob/{}/{}'.format(returnDict[key]['jobtype'], returnDict[key]['jobid'])
+        }
         returnDict[key].pop('process', -1)
 
     return Response(json.dumps(returnDict), mimetype='text/json')
 
+@app.route('/api/restartJob/<string:type>/<int:jobId>')
+def restartJob(type, jobId):
+    jobFile = getJobParamFilename(type, jobId)
+    if not os.path.exists(jobFile):
+        return Response(json.dumps({'error': '-1'}), mimetype='text/json')
+    
+    with open(jobFile, 'r') as f:
+        params = json.load(f)
+    
+    result = dict()
+    if type == 'model':
+        result = triggerModelPublish(jobId, params['repo'])
+
+    return Response(json.dumps(result), mimetype='text/json')
+
 @app.route('/api/publishModel', methods=['POST'])
 def runModelPublish():
     jobId = random.randint(1, 99999)
-    jobFile = os.path.join(ConstantPaths.LUIGI_TASK_PATH, 'model_publish_{}.json'.format(jobId))
+    jobFile = getJobParamFilename('model', jobId)
     while os.path.exists(jobFile):
         jobId = random.randint(1, 99999)
-        jobFile = os.path.join(ConstantPaths.LUIGI_TASK_PATH, 'model_publish_{}.json'.format(jobId))
+        jobFile = getJobParamFilename('model', jobId)
     with open(jobFile, 'w') as f:
         f.write(json.dumps(request.form))
 
-    triggerModelPublish(jobId, request.form['repo']) # Async
-
-    ret = {
-        'id': jobId,
-        'type': 'model_publish',
-        'status': StatusCode.RUNNING,
-        'links': {
-            'get_status': '/api/jobStatus/model/{}'.format(jobId),
-            'kill_job': '/api/killJob/model/{}'.format(jobId)
-        }
-    }
+    ret = triggerModelPublish(jobId, request.form['repo']) # Async
 
     return Response(json.dumps(ret), mimetype='text/json')
 
@@ -95,7 +194,7 @@ def runModelPublish():
 def jobStatus(type, jobId):
     ret = {
         'id': jobId,
-        'type': type + '_publish',
+        'type': type,
         'status': StatusCode.RUNNING,
         'links': {
             'get_status': '/api/jobStatus/{}/{}'.format(type, jobId),
@@ -127,8 +226,12 @@ def cleanJobs(type, forcekill):
         if forcekill and status is None:
             killed += 1
             workflow['process'].terminate()
+            removeQuiet(getJobParamFilename(type, workflow['jobid']))
+            deleteJobFromDb(type, workflow['jobid'])
             runningWorkflows.pop(key, -1)
-        if status is not None:
+        elif status is not None:
+            removeQuiet(getJobParamFilename(type, workflow['jobid']))
+            deleteJobFromDb(type, workflow['jobid'])
             runningWorkflows.pop(key, -1)
     return Response(json.dumps({ 'jobs_killed': killed, 'jobs_cleaned': firstSize - len(runningWorkflows) }), mimetype='text/json')
 
@@ -137,7 +240,7 @@ def killJob(type, jobId):
     key = "{}_{}".format(type, jobId)
     ret = {
         'id': jobId,
-        'type': type + '_publish',
+        'type': type,
         'status': StatusCode.RUNNING,
         'links': {
             'get_status': '/api/jobStatus/{}/{}'.format(type, jobId),
@@ -155,6 +258,8 @@ def killJob(type, jobId):
             ret['status'] = StatusCode.FAILED
         else:
             ret['status'] = StatusCode.ALREADY_COMPLETE
+    removeQuiet(getJobParamFilename(type, jobId))
+    deleteJobFromDb(type, jobId)
     runningWorkflows.pop(key, -1)
     return Response(json.dumps(ret), mimetype='text/json')
 
@@ -166,9 +271,27 @@ def triggerModelPublish(jobId, repo):
     os.chdir(ConstantPaths.LUIGI_TASK_PATH)
     # subprocess.run(['mayapy', os.path.abspath(os.path.join(ConstantPaths.LUIGI_TASK_PATH, 'runTasks.py')), str(jobId)], stderr=subprocess.STDOUT, check=True)
     proc = subprocess.Popen(['mayapy', os.path.abspath(os.path.join(ConstantPaths.LUIGI_TASK_PATH, 'runTasks.py')), str(jobId)], stderr=subprocess.STDOUT)
-    runningWorkflows["{}_{}".format('model', jobId)] = {
+    
+    workflow = {
         'process': proc,
-        'jobId': jobId,
-        'type': 'Model Publish'
+        'jobid': jobId,
+        'jobtype': 'model',
+        'jobname': 'model_publish',
+        'machinename': HOSTNAME,
+        'jobstatus': 0,
+        'triggeredby': 'Anonymous',
+        'taskstatus': 'Doing things...',
+        'executorip': socket.gethostbyname(HOSTNAME)
     }
+    runningWorkflows["{}_{}".format('model', jobId)] = workflow
     os.chdir(cwd)
+    updateRemoteWorkflowInfo('model', jobId, workflow, True)
+    return {
+        'id': jobId,
+        'type': 'model',
+        'status': StatusCode.RUNNING,
+        'links': {
+            'get_status': '/api/jobStatus/model/{}'.format(jobId),
+            'kill_job': '/api/killJob/model/{}'.format(jobId)
+        }
+    }
--- static/main.js
+++ static/main.js
@@ -13,25 +13,76 @@ const capitalize = str => {
     return str.split(' ').map(word => word[0].toUpperCase() + word.slice(1).toLowerCase()).join(' ');
 }
 
+const killJob = async url => {
+    const response = await fetch(url);
+    const json = await response.json();
+    await loadJobList();
+}
+
+const restartJob = async (type, jobId) => {
+    const response = await fetch(`/api/restartJob/${type}/${jobId}`);
+    const json = await response.json();
+    console.log(json);
+    await loadJobList();
+}
+
 const loadJobList = async () => {
     const response = await fetch('/api/getTrackedJobs');
     const json = await response.json();
     mainDiv.innerHTML = '';
     let output = `
     <div class="row">
-        <div class="cell header">Abort</div>
+        <div class="cell header">Actions</div>
         <div class="cell header">Job ID</div>
         <div class="cell header">Type</div>
         <div class="cell header">Status</div>
+        <div class="cell header">Machine</div>
     `;
-    for (let [key, workflow] of Object.entries(json)) {
+    for (let workflow of Object.values(json)) {
         output += `
-            <div class="cell">X</div>
-            <div class="cell">${workflow.jobId}</div>
-            <div class="cell">${workflow.type}</div>
-            <div class="cell">${capitalize(Status[workflow.status])}</div>
+            <div class="cell"><span class="link" id="killLink${workflow.jobid}">X</span> <span class="link" id="restartLink${workflow.jobid}">🔃</span></div>
+            <div class="cell">${workflow.jobid}</div>
+            <div class="cell">${capitalize(workflow.jobtype)}</div>
+            <div class="cell">${capitalize(Status[workflow.jobstatus])}</div>
+            <div class="cell">${workflow.machinename} (${workflow.executorip})</div>
         `;
     }
     mainDiv.innerHTML = output + '</div>';
+    for (let workflow of Object.values(json)) {
+        document.getElementById(`killLink${workflow.jobid}`).addEventListener('click', () => {
+            killJob(workflow.links.kill_job)
+        });
+
+        document.getElementById(`restartLink${workflow.jobid}`).addEventListener('click', () => {
+           restartJob(workflow.type, workflow.jobId)
+        });
+    }
 }
-loadJobList()
+
+loadJobList();
+let reloadTimer = null;
+
+loadJobList();
+const autoReloadCheckbox = document.getElementById('autoReload');
+
+const setAutoReload = async enabled => {
+    autoReloadCheckbox.checked = enabled;
+    if (enabled) {
+        await loadJobList();
+        reloadTimer = setInterval(loadJobList, 5000);
+    } else {
+        clearInterval(reloadTimer);
+    }
+    window.localStorage.setItem('autoReload', enabled ? '1' : '0');
+}
+
+autoReloadCheckbox.addEventListener('change', async () => {
+    setAutoReload(autoReloadCheckbox.checked);
+});
+
+const reloadSetting = window.localStorage.getItem('autoReload');
+if (reloadSetting === null) {
+    setAutoReload(true);
+} else {
+    setAutoReload(reloadSetting === '1');
+}
